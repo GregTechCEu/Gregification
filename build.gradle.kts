3941af5ec3503f1d143d4f3784ddbe27e27486b2
DStrand1 <dane@strandboge.com>
2021-06-27 17:02:55 -0500

    update buildscript

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import net.minecraftforge.gradle.user.UserBaseExtension
+import java.util.*
+
+buildscript {
+    repositories {
+        mavenCentral()
+        maven {
+            name = "Jitpack"
+            setUrl("https://jitpack.io")
+        }
+        maven {
+            name = "Forge"
+            setUrl("https://maven.minecraftforge.net")
+        }
+    }
+    dependencies {
+        classpath("com.github.GregTechCE:ForgeGradle:FG_2.3-SNAPSHOT")
+    }
+}
+
+apply {
+    plugin("net.minecraftforge.gradle.forge")
+}
+
+val Project.minecraft: UserBaseExtension
+    get() = extensions.getByName<UserBaseExtension>("minecraft")
+
+val config: Properties = file("build.properties").inputStream().let {
+    val prop = Properties()
+    prop.load(it)
+    return@let prop
+}
+
+val modVersion = config["gregification.version"] as String
+val mcVersion = config["mc.version"] as String
+val forgeVersion = "$mcVersion-${config["forge.version"]}"
+val shortVersion = mcVersion.substring(0, mcVersion.lastIndexOf("."))
+val strippedVersion = shortVersion.replace(".", "") + "0";
+
+version = "$mcVersion-$modVersion"
+group = "gregification"
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+configure<BasePluginConvention> {
+    archivesBaseName = "Gregification"
+}
+
+configure<UserBaseExtension> {
+    version = forgeVersion
+    mappings = config["mcp.version"] as String
+    runDir = "run"
+    //replace("@VERSION@", modVersion)
+    //replaceIn("Gregification.java")
+}
+
+repositories {
+}
+
+dependencies {
+}
+
+val processResources: ProcessResources by tasks
+val sourceSets: SourceSetContainer = the<JavaPluginConvention>().sourceSets
+
+processResources.apply {
+    inputs.property("version", modVersion)
+    inputs.property("mcversion", forgeVersion)
+
+    from(sourceSets["main"].resources.srcDirs) {
+        include("mcmod.info")
+        expand(mapOf("version" to modVersion, "mcversion" to forgeVersion))
+    }
+
+    from(sourceSets["main"].resources.srcDirs) {
+        exclude("mcmod.info")
+    }
+}
