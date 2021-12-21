aa566d10037a195609c6bb49f332f3a8eed72b34
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2021-12-22 00:52:51 +0100

    add Tree Seeds

--- src/main/java/gregification/exnihilo/CustomDrops.java
+++ src/main/java/gregification/exnihilo/CustomDrops.java
@@ -2,6 +2,7 @@ package gregification.exnihilo;
 
 import exnihilocreatio.ModItems;
 import exnihilocreatio.blocks.BlockSieve;
+import exnihilocreatio.items.seeds.ItemSeedBase;
 import exnihilocreatio.registries.manager.ISieveDefaultRegistryProvider;
 import exnihilocreatio.registries.registries.SieveRegistry;
 import exnihilocreatio.util.ItemInfo;
@@ -30,6 +31,9 @@ public class CustomDrops implements ISieveDefaultRegistryProvider {
         registry.register("dirt", new ItemInfo(Items.PUMPKIN_SEEDS), 0.35f, BlockSieve.MeshType.STRING.getID());
         registry.register("dirt", new ItemInfo(ModItems.resources, 3), 0.05f, BlockSieve.MeshType.STRING.getID());
         registry.register("dirt", new ItemInfo(ModItems.resources, 4), 0.05f, BlockSieve.MeshType.STRING.getID());
+        for (ItemSeedBase seed : ModItems.itemSeeds)  {
+            registry.register("dirt", new ItemInfo(seed), 0.05f, BlockSieve.MeshType.STRING.getID());
+        }
         registry.register("dust", new ItemInfo(Items.DYE, 15), 0.1f, BlockSieve.MeshType.STRING.getID());
         registry.register("dust", new ItemInfo(Items.GLOWSTONE_DUST), 0.025f, BlockSieve.MeshType.IRON.getID());
         registry.register("dust", new ItemInfo(Items.BLAZE_POWDER), 0.037f, BlockSieve.MeshType.IRON.getID());
