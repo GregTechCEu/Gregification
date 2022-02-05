9c723cfa84d16cbea8a9896f2a2824326fcfc6ec
DStrand1 <dane@strandboge.com>
2022-02-05 01:03:40 -0600

    Ores for comb recipes

--- src/main/java/gregification/common/GFOrePrefix.java
+++ src/main/java/gregification/common/GFOrePrefix.java
@@ -5,6 +5,8 @@ import gregtech.api.GregTechAPI;
 import gregtech.api.unification.material.Materials;
 import gregtech.api.unification.material.info.MaterialFlags;
 import gregtech.api.unification.material.info.MaterialIconType;
+import gregtech.api.unification.material.properties.OreProperty;
+import gregtech.api.unification.material.properties.PropertyKey;
 import gregtech.api.unification.ore.OrePrefix;
 import gregtech.common.items.MetaItems;
 import net.minecraftforge.fml.common.Loader;
@@ -74,5 +76,79 @@ public class GFOrePrefix {
         Materials.Lithium.addFlags(MaterialFlags.FORCE_GENERATE_BLOCK);
         Materials.Electrotine.addFlags(MaterialFlags.FORCE_GENERATE_BLOCK);
         Materials.Lutetium.addFlags(MaterialFlags.FORCE_GENERATE_BLOCK);
+
+        // Ores for Comb Processing
+        OreProperty property;
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Phosphorus);
+        Materials.Phosphate.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Iron, Materials.Magnesium);
+        Materials.Chrome.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Chrome, Materials.Iron);
+        Materials.Manganese.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Olivine);
+        Materials.Magnesium.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.SiliconDioxide);
+        Materials.Silicon.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Tin, Materials.Gallium);
+        Materials.Zinc.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Zinc, Materials.Iron);
+        Materials.Antimony.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Manganese, Materials.Molybdenum);
+        Materials.Tungsten.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Almandine);
+        Materials.Titanium.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Iridium);
+        Materials.Osmium.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Platinum, Materials.Osmium);
+        Materials.Iridium.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Gold, Materials.Silver);
+        Materials.Electrum.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Lead, Materials.Uranium235, Materials.Thorium);
+        Materials.Uranium238.setProperty(PropertyKey.ORE, property);
+
+        Materials.Uranium235.setProperty(PropertyKey.ORE, new OreProperty());
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Naquadah, Materials.Naquadria);
+        Materials.NaquadahEnriched.setProperty(PropertyKey.ORE, property);
+
+        property = new OreProperty();
+        property.setOreByProducts(Materials.Neutronium);
+        Materials.Neutronium.setProperty(PropertyKey.ORE, property);
+
+        Materials.Gallium.setProperty(PropertyKey.ORE, new OreProperty());
+        Materials.Niobium.setProperty(PropertyKey.ORE, new OreProperty());
+        Materials.Arsenic.setProperty(PropertyKey.ORE, new OreProperty());
+        Materials.Bismuth.setProperty(PropertyKey.ORE, new OreProperty());
+        Materials.Rutile.setProperty(PropertyKey.ORE, new OreProperty());
+        Materials.Naquadria.setProperty(PropertyKey.ORE, new OreProperty());
+        Materials.Lutetium.setProperty(PropertyKey.ORE, new OreProperty());
+        Materials.Americium.setProperty(PropertyKey.ORE, new OreProperty());
     }
 }
