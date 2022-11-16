df425bcfcd53d7ef11c987e9b2f8ef7a41523389
DStrand1 <dane@strandboge.com>
2022-11-15 18:20:42 -0600

    improve blast metal melting implementation

--- src/main/java/gregification/modules/tinkers/TinkersEvents.java
+++ src/main/java/gregification/modules/tinkers/TinkersEvents.java
@@ -1,37 +1,51 @@
 package gregification.modules.tinkers;
 
 import gregtech.api.GregTechAPI;
+import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.properties.PropertyKey;
+import gregtech.api.unification.ore.OrePrefix;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import slimeknights.mantle.util.RecipeMatch;
 import slimeknights.tconstruct.library.events.TinkerRegisterEvent.MeltingRegisterEvent;
 import slimeknights.tconstruct.library.smeltery.MeltingRecipe;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import static gregtech.api.unification.ore.OrePrefix.*;
 
 public class TinkersEvents {
 
-    private static List<Fluid> blastMaterials;
+    private static Map<Fluid, Material> blastMaterials;
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onMeltingRegisterEvent(MeltingRegisterEvent event) {
         MeltingRecipe recipe = event.getRecipe();
-        if (getBlastMaterials().contains(recipe.getResult().getFluid())) {
+        Material m = getBlastMaterials().get(recipe.getResult().getFluid());
+        if (m != null && (matches(recipe.input, ore, m) || matches(recipe.input, dust, m))) {
             event.setCanceled(true);
         }
     }
 
-    private static List<Fluid> getBlastMaterials() {
-        if (blastMaterials != null) return blastMaterials;
-        blastMaterials = new ArrayList<>();
-        for (Material m : GregTechAPI.MATERIAL_REGISTRY) {
-            if (m.hasProperty(PropertyKey.BLAST) && m.hasProperty(PropertyKey.FLUID)) {
-                blastMaterials.add(m.getFluid());
+    private static Map<Fluid, Material> getBlastMaterials() {
+        Map<Fluid, Material> map = blastMaterials;
+        if (map == null) {
+            map = new HashMap<>();
+            for (Material m : GregTechAPI.MATERIAL_REGISTRY) {
+                if (m.hasProperty(PropertyKey.BLAST) && m.hasProperty(PropertyKey.FLUID)) {
+                    map.put(m.getFluid(), m);
+                }
             }
+            blastMaterials = map;
         }
-        return blastMaterials;
+        return map;
+    }
+
+    private static boolean matches(RecipeMatch input, OrePrefix p, Material m) {
+        return input.matches(NonNullList.withSize(1, OreDictUnifier.get(p, m))).isPresent();
     }
 }
