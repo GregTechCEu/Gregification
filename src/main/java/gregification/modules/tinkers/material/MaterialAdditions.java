a4c243c9c0c00eee410ccd60c2192d58c8db55e6
DStrand1 <dane@strandboge.com>
2023-01-11 15:12:00 -0600

    update for 2.5.0

--- src/main/java/gregification/modules/tinkers/material/MaterialAdditions.java
+++ src/main/java/gregification/modules/tinkers/material/MaterialAdditions.java
@@ -29,75 +29,38 @@ public class MaterialAdditions {
 
         // Cancellations, to avoid autogen nastiness
         MaterialProcessing.getBuilder(Materials.Diamond).cancel();
-        MaterialProcessing.getBuilder(Materials.Emerald).cancel();
 
         // Modifications
-        MaterialProcessing.getBuilder(Materials.Opal);
-        MaterialProcessing.getBuilder(Materials.Invar);
-        MaterialProcessing.getBuilder(Materials.Duranium);
-        MaterialProcessing.getBuilder(Materials.StainlessSteel);
-        MaterialProcessing.getBuilder(Materials.Titanium);
-        MaterialProcessing.getBuilder(Materials.TungstenSteel);
-        MaterialProcessing.getBuilder(Materials.Naquadah);
-        MaterialProcessing.getBuilder(Materials.VanadiumSteel);
-        MaterialProcessing.getBuilder(Materials.BismuthBronze);
-        MaterialProcessing.getBuilder(Materials.Osmium);
-        MaterialProcessing.getBuilder(Materials.Olivine);
-        MaterialProcessing.getBuilder(Materials.BlackSteel);
-        MaterialProcessing.getBuilder(Materials.HSSS);
-        MaterialProcessing.getBuilder(Materials.GreenSapphire);
-        MaterialProcessing.getBuilder(Materials.SterlingSilver);
-        MaterialProcessing.getBuilder(Materials.RhodiumPlatedPalladium);
-        MaterialProcessing.getBuilder(Materials.HSSG);
-        MaterialProcessing.getBuilder(Materials.Topaz);
+        MaterialProcessing.getBuilder(Materials.Flint);
+        MaterialProcessing.getBuilder(Materials.Iron);
+        MaterialProcessing.getBuilder(Materials.Bronze);
         MaterialProcessing.getBuilder(Materials.WroughtIron);
-        MaterialProcessing.getBuilder(Materials.GarnetRed);
-        MaterialProcessing.getBuilder(Materials.Tungsten);
-        MaterialProcessing.getBuilder(Materials.Chrome);
-        MaterialProcessing.getBuilder(Materials.Neutronium);
-        MaterialProcessing.getBuilder(Materials.Ruby);
-        MaterialProcessing.getBuilder(Materials.GarnetYellow);
-        MaterialProcessing.getBuilder(Materials.RoseGold);
-        MaterialProcessing.getBuilder(Materials.HSSE);
+        MaterialProcessing.getBuilder(Materials.Invar);
         MaterialProcessing.getBuilder(Materials.DamascusSteel);
+
+        MaterialProcessing.getBuilder(Materials.Steel);
+        MaterialProcessing.getBuilder(Materials.CobaltBrass);
+
+        MaterialProcessing.getBuilder(Materials.Aluminium);
+        MaterialProcessing.getBuilder(Materials.VanadiumSteel);
+        MaterialProcessing.getBuilder(Materials.SterlingSilver);
+        MaterialProcessing.getBuilder(Materials.RoseGold);
+
+        MaterialProcessing.getBuilder(Materials.StainlessSteel);
+        MaterialProcessing.getBuilder(Materials.BlueSteel);
+        MaterialProcessing.getBuilder(Materials.RedSteel);
+        // MaterialProcessing.getBuilder(Materials.NeodymiumMagnetic);
+
+        MaterialProcessing.getBuilder(Materials.Titanium);
+        MaterialProcessing.getBuilder(Materials.Ultimet);
+
+        MaterialProcessing.getBuilder(Materials.TungstenSteel);
+        MaterialProcessing.getBuilder(Materials.HSSE);
         MaterialProcessing.getBuilder(Materials.TungstenCarbide);
 
-        MaterialProcessing.getBuilder(Materials.Uranium238)
-                .addTrait(TRAIT_RADIOACTIVE, HEAD, BOW, SHAFT, PROJECTILE);
-
-        MaterialProcessing.getBuilder(Materials.Osmiridium);
-        MaterialProcessing.getBuilder(Materials.BlueSteel);
-        MaterialProcessing.getBuilder(Materials.Manganese);
-        MaterialProcessing.getBuilder(Materials.Sapphire);
-        MaterialProcessing.getBuilder(Materials.Amethyst);
-        MaterialProcessing.getBuilder(Materials.Aluminium);
-        MaterialProcessing.getBuilder(Materials.BlackBronze);
-        MaterialProcessing.getBuilder(Materials.Palladium);
-        MaterialProcessing.getBuilder(Materials.CobaltBrass);
-        MaterialProcessing.getBuilder(Materials.Molybdenum);
         MaterialProcessing.getBuilder(Materials.NaquadahAlloy);
-
-        MaterialProcessing.getBuilder(Materials.Thorium)
-                .addTrait(TRAIT_RADIOACTIVE, HEAD, BOW, SHAFT, PROJECTILE);
-
-        MaterialProcessing.getBuilder(Materials.Brass);
-
-        MaterialProcessing.getBuilder(Materials.Neodymium)
-                .addTrait(magnetic2, HEAD);
-
-        MaterialProcessing.getBuilder(Materials.Uranium235)
-                .addTrait(TRAIT_RADIOACTIVE, HEAD, BOW, SHAFT, PROJECTILE);
-
-        MaterialProcessing.getBuilder(Materials.Tritanium);
-
-        MaterialProcessing.getBuilder(Materials.NaquadahEnriched)
-                .addTrait(TRAIT_RADIOACTIVE, HEAD, BOW, SHAFT, PROJECTILE);
-
-        MaterialProcessing.getBuilder(Materials.RedSteel);
-        MaterialProcessing.getBuilder(Materials.Iridium);
-        MaterialProcessing.getBuilder(Materials.Ultimet);
-        MaterialProcessing.getBuilder(Materials.BlueTopaz);
-        MaterialProcessing.getBuilder(Materials.Magnalium);
+        MaterialProcessing.getBuilder(Materials.Duranium);
+        MaterialProcessing.getBuilder(Materials.Neutronium);
 
         // Additions
 
